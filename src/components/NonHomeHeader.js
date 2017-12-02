ce1f4546c5b1e8b82f7553ab9420bcfba0fc3653
Jawad Khawaja <devjawkha@gmail.com>
2017-12-02 20:42:48 +0100

    minor styling

--- src/components/NonHomeHeader.js
+++ src/components/NonHomeHeader.js
@@ -3,10 +3,10 @@ import TextLogo from './../assets/images/logos/quinoa_logo_text.jpg';
 
 const NonHomeHeader = () => {
     return (
-        <header className="text-center m5">
+        <header className="text-center">
         <img className="img-fluid" src={TextLogo} alt='quinoa'/>
       </header>
-    )
+    );
 }
 
 export default NonHomeHeader;
