03ea42db9ae23d76f047a41b1e3c33265d691498
Jawad Khawaja <devjawkha@gmail.com>
2018-01-04 10:44:58 +0100

    beginning to rebuild from top down

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import Home from './components/Home';
-import TaggedCategories from './components/TaggedCategories'; 
+// COMPONENTS
+import Header from './components/Header';
+import Home from './Home/Home';
+import About from './About/About';
+import ApplyNow from './Apply/Apply';
+import Support from './Support';
+import Contact from './Contact';
+import DonateModal from './DonateModal';
+import NotFound from './NotFound';
 
-class App extends Component {
-  constructor() {
-    super();
-  }
 
-  render() {
-    return (
-      <div className="App">
-        <Home />
-        <TaggedCategories />        
-      </div>
-    );
-  }
-}
+const App = () => {
+	return (
+		<BrowserRouter>
+			<div>
+				<Header />
+				<Switch>
+					<Route exact path="/" component={Home} />
+					<Route path="/about" component={About} />
+					<Route path="/apply" component={ApplyNow} />
+					<Route path="/support" component={Support} />
+					<Route path="/contact" component={Contact} />
+					<Route path="/donate" component={DonateModal} />
+					<Route component={NotFound} />
+				</Switch>
+			</div>
+		</BrowserRouter>
+	);
+};
 
 export default App;
