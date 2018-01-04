870a4556724da23c5d739a4e29376013606f2232
Jawad Khawaja <devjawkha@gmail.com>
2018-01-04 18:48:22 +0100

    defined app-wide action types

--- src/actions/ActionTypes.js
+++ src/actions/ActionTypes.js
@@ -2,6 +2,8 @@ const ActionTypes = {
 	GET_TAGS: 'GET_TAGS',
 	SET_ACTIVE_TAG: 'SET_ACTIVE_TAG',
 	GET_CATEGORIES: 'GET_CATEGORIES',
+	GET_PRODUCTS: 'GET_PRODUCTS',
+	GET_SHOPS: 'GET_SHOPS',
 };
 
 export default ActionTypes;
